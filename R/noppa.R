b0ebef0c0a1297403da44bbf80706ba5c0d04e1a
Johannes <jtraja@utu.fi>
2023-11-17 23:34:08 +0200

    Added parameter to turn plotting off, and it now defaults to false. Also added correct dependencies.

--- R/noppa.R
+++ R/noppa.R
@@ -5,15 +5,19 @@
 #' @param sivuja Nopan sivujen lukumäärä (oletuksena 6).
 #' @param lkm Heittojen lukumäärä (oletuksena 1).
 #' @param takaisinpalautus Onko nopanheitot takaisinpalautuvia, eli toisistaan riippumattomia (oletuksena TRUE).
+#' @param plot Tehdäänkö kuvaaja vai ei (oletuksena FALSE).
 #'
-#' @return Tulostaa nopanheitot ja niiden summan.
+#' @return Tulostaa nopanheitot ja niiden summan, sekä summan pistetodennäköisyysfunktion.
+#'
+#' @importFrom stats convolve
+#' @importFrom graphics points legend
 #'
 #' @examples
-#' # Heitä noppaa 5 kertaa
+#' # Heitä perinteistä 6 sivuista noppaa 5 kertaa
 #' noppa(6, 5)
 #'
 #' @export
-noppa = function(sivuja = 6, lkm = 1, takaisinpalautus = TRUE){
+noppa = function(sivuja = 6, lkm = 1, plot = FALSE,  takaisinpalautus = TRUE){
   heitot = sample(seq(sivuja), lkm, replace = takaisinpalautus)
   cat("Heitit:", heitot)
   if(lkm > 1) cat("\nSumma:", sum(heitot),"\n")
@@ -24,11 +28,14 @@ noppa = function(sivuja = 6, lkm = 1, takaisinpalautus = TRUE){
     for(i in 2:lkm){
       pdf = convolve(pdf, rep(1/sivuja, sivuja), type = "open")
     }
+    if(plot == TRUE){
     # plot the pmf
     plot(seq(lkm, lkm*sivuja), pdf, type = "b", main = "Nopanheiton summan jakauma", xlab = "Summa", ylab = "Todennäköisyys")
-    # plot the obtained result as well as a vertical line at the obtained result
+    # plot the obtained result
     points(sum(heitot), pdf[sum(heitot)-(lkm-1)], col = "red", pch = 19)
     # add a legend and the probability of P(sum <= ) the obtained result in a box
     legend("topleft", legend = paste0("P(summa ", lkm, "d", sivuja,  " <= ", sum(heitot), ") = ", round(sum(pdf[1:(sum(heitot)-(lkm-1))]), 3)), bty = "n")
+    }
+    cat(paste0("P(summa ", lkm, "d", sivuja,  " <= ", sum(heitot), ") = ", round(sum(pdf[1:(sum(heitot)-(lkm-1))]), 3)))
   }
 }
