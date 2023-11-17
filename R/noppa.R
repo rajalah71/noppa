8febd6098b7272ae462174413a47e8e7135d69e8
Johannes <johannes@kone.koti>
2023-11-17 17:29:49 +0200

    Refined functionality

--- R/noppa.R
+++ R/noppa.R
@@ -1,4 +1,20 @@
-noppa = function(sivuja, lkm = 1, takaisinpalautus = TRUE){
+#' Noppaheitto
+#'
+#' Simuloi nopanheiton.
+#'
+#' @param sivuja Nopan sivujen lukumäärä (oletuksena 6).
+#' @param lkm Heittojen lukumäärä (oletuksena 1).
+#' @param takaisinpalautus Onko nopanheitot takaisinpalautuvia (oletuksena TRUE).
+#'
+#' @return Tulostaa nopanheitot ja niiden summan.
+#'
+#' @examples
+#' # Heitä noppaa 5 kertaa
+#' noppa(6, 5)
+#'
+#' @export
+noppa = function(sivuja = 6, lkm = 1, takaisinpalautus = TRUE){
   heitot = sample(seq(sivuja), lkm, replace = takaisinpalautus)
-  cat("Heitit:", heitot, ". Summa:", sum(heitot))
+  cat("Heitit:", heitot)
+  if(length(heitot) > 1) cat("\nSumma:", sum(heitot),"\n")
 }
