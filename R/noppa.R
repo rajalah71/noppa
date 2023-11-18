dbd34bf8659f75406eb8896f4e6c4090b8efe6d3
Johannes <johannes@kone.koti>
2023-11-18 12:41:23 +0200

    Changed parameter order. Version 3.0.0

--- R/noppa.R
+++ R/noppa.R
@@ -1,6 +1,8 @@
-#' Noppaheitto
+#' @title Noppaheitto
 #'
-#' Simuloi nopanheiton.
+#' @description
+#' Heittää k kertaa n sivuista noppaa, Heitot, niiden summa ja todennäköisyys tulostetaan.
+#'
 #'
 #' @param sivuja Nopan sivujen lukumäärä (oletuksena 6).
 #' @param lkm Heittojen lukumäärä (oletuksena 1).
@@ -13,30 +15,31 @@
 #' @importFrom graphics points legend
 #'
 #' @examples
-#' # Heitä perinteistä 6 sivuista noppaa 5 kertaa
-#' noppa(6, 5)
+#' # Heitä 2 kertaa perinteistä 6 sivuista noppaa.
+#' noppa(2,6)
 #'
 #' @export
-noppa = function(sivuja = 6, lkm = 1, plot = FALSE,  takaisinpalautus = TRUE){
+noppa = function(lkm = 1, sivuja = 20, plot = FALSE,  takaisinpalautus = TRUE){
   heitot = sample(seq(sivuja), lkm, replace = takaisinpalautus)
-  cat("Heitit:", heitot)
+  # cat the results separated by commas
+  cat("Heitit:", paste(heitot, collapse = ", "))
   if(lkm > 1) cat("\nSumma:", sum(heitot),"\n")
 
   if(lkm > 1){
-    # Calculate the pdf of the sum
-    pdf = rep(1/sivuja, sivuja)
+    # Calculate the pmf of the sum
+    pmf = rep(1/sivuja, sivuja)
     for(i in 2:lkm){
-      pdf = convolve(pdf, rep(1/sivuja, sivuja), type = "open")
+      pmf = convolve(pmf, rep(1/sivuja, sivuja), type = "open")
     }
-    cat(paste0("P(summa ", lkm, "d", sivuja,  " <= ", sum(heitot), ") = ", round(sum(pdf[1:(sum(heitot)-(lkm-1))]), 3)))
+    cat(paste0("P(", lkm, "d", sivuja,  " <= ", sum(heitot), ") = ", round(sum(pmf[1:(sum(heitot)-(lkm-1))]), 3)), "\n")
 
     if(plot == TRUE){
     # plot the pmf
-    plot(seq(lkm, lkm*sivuja), pdf, type = "b", main = "Nopanheiton summan jakauma", xlab = "Summa", ylab = "Todennäköisyys")
+    plot(seq(lkm, lkm*sivuja), pmf, type = "b", main = "Nopanheiton summan jakauma", xlab = "Summa", ylab = "Todennäköisyys")
     # plot the obtained result
-    points(sum(heitot), pdf[sum(heitot)-(lkm-1)], col = "red", pch = 19)
+    points(sum(heitot), pmf[sum(heitot)-(lkm-1)], col = "red", pch = 19)
     # add a legend and the probability of P(sum <= ) the obtained result in a box
-    legend("topleft", legend = paste0("P(summa ", lkm, "d", sivuja,  " <= ", sum(heitot), ") = ", round(sum(pdf[1:(sum(heitot)-(lkm-1))]), 3)), bty = "n")
+    legend("topleft", legend = paste0("P(", lkm, "d", sivuja,  " <= ", sum(heitot), ") = ", round(sum(pmf[1:(sum(heitot)-(lkm-1))]), 3)), bty = "n")
     }
   }
 }
